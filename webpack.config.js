2477758567da53db15338175d95bbe2500bc1b24
AHmims <ali.hmims99@gmail.com>
2021-05-03 22:11:05 +0000

    revert to 1bf0a27852c1c5bc00ba764bdf03d92198425794

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
+   mode: 'production',
+   entry: './src/js/script.js',
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+   },
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  presets: ['@babel/preset-env'],
+               },
             },
-            {
-                test: /\.css$/,
-                include: path.resolve(__dirname, 'src'),
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
-            },
-        ],
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
-        watchContentBase: true,
-    },
+         },
+         {
+            test: /\.css$/,
+            include: path.resolve(__dirname, 'src'),
+            use: ['style-loader', 'css-loader', 'postcss-loader'],
+         },
+      ],
+   },
+   devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      watchContentBase: true,
+   }
 };
