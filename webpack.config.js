f5460d7d55fd3515ed83637519f6d8ae679ebda7
AHmims <ali.hmims99@gmail.com>
2021-05-03 22:07:34 +0000

    revert last commit

--- webpack.config.js
+++ webpack.config.js
@@ -6,28 +6,5 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                include: path.resolve(__dirname, 'src'),
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
-            },
-        ],
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
-        watchContentBase: true,
-    },
+    }
 };
